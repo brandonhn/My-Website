34577f191f6e0e11ecd950cefe53e4e416101b22
brandonhn <61365182+brandonhn@users.noreply.github.com>
2020-07-22 11:25:39 -0700

    Add files via upload

---
+++ typewriter.js
@@ -0,0 +1,49 @@
+const TypeWriter = function(txtElement, words, wait = 3000) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = '';
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+}
+
+TypeWriter.prototype.type = function() {
+  const current = this.wordIndex % this.words.length;
+  const fulltxt = this.words[current];
+
+  if(this.isDeleting) {
+    this.txt = fulltxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fulltxt.substring(0, this.txt.length + 1);
+  }
+
+  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+  let typeSpeed = 80;
+
+  if(this.isDeleting) {
+    typeSpeed = 60;
+  }
+
+  if(!this.isDeleting && this.txt === fulltxt){
+    typeSpeed = this.wait;
+    this.isDeleting = true;
+  } else if(this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.wordIndex++;
+    typeSpeed = 150;
+  }
+
+  setTimeout(() => this.type(), typeSpeed)
+}
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+  const txtElement = document.querySelector('.txt-type');
+  const words = JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+
+  new TypeWriter(txtElement, words, wait);
+}
